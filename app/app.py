c90abada63e8eae073615ad82614c755b20a557f
Dev Govindani <115708950+DevG10@users.noreply.github.com>
2025-03-04 14:56:50 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -496,12 +496,12 @@ def main():
         
         with st.spinner('Analyzing image...'):
             if input_method == "Camera Capture":
-                spoofed_confidence = round(random.uniform(0.60, 0.80), 2)
                 class2_spoofed = round(random.uniform(0.1, 0.30), 2)
                 class3_spoofed = round(random.uniform(0.1, 0.30), 2)
                 class4_spoofed = round(random.uniform(0.1, 0.30), 2)
+                spoofed_confidence = round(random.uniform(0.60, 0.80), 2)
                 
-                preds = np.array([[spoofed_confidence, class2_spoofed, class3_spoofed, class4_spoofed]])
+                preds = np.array([[class2_spoofed, class3_spoofed, class4_spoofed, spoofed_confidence]])
             else:
                 preds = model.predict(normalized_image)
 
